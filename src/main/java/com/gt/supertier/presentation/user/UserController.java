30eb9c314703bbbc0ada76efdceb85c901fb728c
Juan Romero Santamaría <jmanuelromero93@gmail.com>
2018-06-01 17:47:58 -0500

    Se agrega listener de select para la tabla de usuario

--- src/main/java/com/gt/supertier/presentation/user/UserController.java
+++ src/main/java/com/gt/supertier/presentation/user/UserController.java
@@ -4,16 +4,31 @@ import com.gt.supertier.business.user.boundary.UserManager;
 import com.gt.supertier.business.user.entitiy.User;
 
 import javax.enterprise.inject.Model;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
 import java.util.List;
 
-@Model
-public class UserController {
+@Named
+@ViewScoped
+public class UserController implements Serializable {
 
     @Inject
     private UserManager userManager;
 
+    private User selectedUser;
+
     public List<User> findAll(){
         return userManager.findAll();
+    }
+
+
+    public User getSelectedUser() {
+        return selectedUser;
+    }
+
+    public void setSelectedUser(User selectedUser) {
+        this.selectedUser = selectedUser;
     }
 }
